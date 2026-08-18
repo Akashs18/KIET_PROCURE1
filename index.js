9cc30f11e2dcb9d00edbbf0c60be41b9bb040b16
Shashank N <shashin1504@gmail.com>
2026-08-18 18:07:35 +0530

    added inventory

--- index.js
+++ index.js
@@ -1346,7 +1346,7 @@ app.get("/api/account-details", async (req, res) => {
         ie.supplier_ifsc_code
       FROM purchase_orders po
       LEFT JOIN inventory_entries ie ON po.id = ie.purchase_order_id
-      WHERE po.status IN ( 'inventory_processed' )
+      WHERE po.status ='inventory_processed'
     `;
     let params = [];
     let paramCount = 0;
